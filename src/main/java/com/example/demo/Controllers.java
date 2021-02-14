410c179b7d66a36e167a35198a461a09cf1a7f34
Thomas SANTONI <thomassantoni@mbp-de-thomas.home>
2021-02-14 20:23:45 +0100

    upload GSB Spring

---
+++ src/main/java/com/example/demo/Controllers.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@Controller
+public class Controllers {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controllers.class, args);
+    }
+
+}
